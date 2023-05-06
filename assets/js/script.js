6936d2819229b47e89acd12709594b042ee6a83f
rona chen <rona.chen@hotmail.com>
2023-05-06 15:11:02 -0400

    added container and added buttons for return and reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,4 +14,3 @@ var startBtn = document.getElementById("#start-button");
 // Function that stores the score and the name to the local storage
 
 // Function that renders the scores and the name on the scoreboard from the local storage
-
