908390f51502926b6306db0a91be00fee30b5da3
rona chen <rona.chen@hotmail.com>
2023-05-06 15:20:38 -0400

    wrote pseudo code that will guide the js code write up

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,30 @@ var startBtn = document.getElementById("#start-button");
 
 
 
+// Start the quiz when we press on the start button
 
+// Hide the start button when the quiz starts
 
+// Start the timer
 
-// Function that diplays question
+// Display questions and answers
 
-// Function that starts the timer
+// If wrong answer, we subtract 10 seconds from the timer
 
-// Function that stores the score and the name to the local storage
+// If wrong answer, change background of the button to red
 
-// Function that renders the scores and the name on the scoreboard from the local storage
+// If right answer, change background of the button to green
+
+// If right or wrong answer, move on to the next question
+
+// Function that stores the score and the name to the local storage: After all the questions are done, we see the score and the submit form 
+
+// Function that renders the scores and the name on the scoreboard from the local storage: After we submit, we the see the past scores
+
+// On the scoreboard, we see reset and return button
+
+// When the reset button is pressed, it clears local storage
+
+// When the return button is pressed, it goes back to the start button screen
+
+// When the scoreboard button is pressed, it shows the highscores
