925557ff4dae5182170895af838d5a19b76acda5
rona chen <rona.chen@hotmail.com>
2023-05-06 19:20:59 -0400

    added a planning js file, and made question container show questions and make buttons for the first array

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,7 +50,7 @@ var questions = [
 var startContainer = document.querySelector(".start-container");
 var quizContainer = document.querySelector(".quiz-container");
 var formContainer = document.querySelector(".form-container");
-var scoreboardContainer = document.querySelector("scoreboard-container");
+var scoreboardContainer = document.querySelector(".scoreboard-container");
 
 // Buttons
 var scoreboardButton = document.querySelector("scoreboard-button");
@@ -62,70 +62,56 @@ var resetButton = document.querySelector(".reset-button");
 
 // Counts
 var timerEl = document.querySelector(".timer-count");
-var scoreEl = document.getElementById("#score-count");
+var scoreEl = document.querySelector(".score-count");
 
 // Others
-var question = document.getElementById("#question");
-var scoreList = document.getElementById("score-list");
+var questionEl= document.querySelector(".question");
+var scoreList = document.querySelector(".score-list");
+
 
 
 
 // Start the quiz when we press on the start button
-startButton.addEventListener("click",startQuiz);
+startButton.addEventListener("click", startQuiz);
 
 
 // Hide the start button when the quiz starts, start timer, and display questions
 function startQuiz() {
-    startButton.setAttribute("style", "display: none");
+    startButton.style.visibility = "hidden";
+    quizContainer.setAttribute("style","display: flex");
     startTimer();
-    // displayQuestion();//TODO make display qs fx 
+    displayQuestion(); 
 }
 
 // Start the timer
 function startTimer() {
-   var timerCount = 50; 
+    var timerCount = 50; 
    var timer = setInterval(function(){
         timerCount--;
         timerEl.textContent = timerCount;
 
     // Test if win conditiuon is met
-    if (quizDone && timerCount > 0) {
-        // showScore();
-        clearInterval(timer);
+    // if (quizDone && timerCount > 0) {
+        // showScore(); //!
+    //     clearInterval(timer);
         
-    }
+    // }
     if (timerCount === 0) {
         clearInterval(timer);
-        // failQuiz();
+        // showScore(); //!
     }
     }, 1000)
 }
 
-//TODO make showscore fx
-//TODO make quizdone
-//TODO fail quiz
 // Display questions and answers
+function displayQuestion() {
+      var currentQuestion = questions[0];
+      questionEl.innerHTML= currentQuestion.q;
 
-// If wrong answer, we subtract 10 seconds from the timer
+    currentQuestion.a.forEach(a => {
+        var choiceButton = document.createElement("button");
+        choiceButton.innerHTML = a.choice;
+        answerButton.appendChild(choiceButton);
+    });   
+}
 
-// If wrong answer, change background of the button to red
-
-// If right answer, change background of the button to green
-
-// If right or wrong answer, move on to the next question
-
-// If time > 0, then score = timeLeft
-
-// If time = 0, and questions = left = failed test ==> start page
-
-// Function that stores the score and the name to the local storage: After all the questions are done, we see the score and the submit form 
-
-// Function that renders the scores and the name on the scoreboard from the local storage: After we submit, we the see the past scores
-
-// On the scoreboard, we see reset and return button
-
-// When the reset button is pressed, it clears local storage
-
-// When the return button is pressed, it goes back to the start button screen
-
-// When the scoreboard button is pressed, it shows the highscores
---
+++ planning.js
@@ -0,0 +1,47 @@
+}
+
+// Check answer selected
+function checkAnswer(event) 
+    
+
+    
+   if selectedAnswer = a.correct {
+        // If right answer, change background of the button to green
+        choiceButton.setAttribute("style", "background-color: green");
+        nextQuestion();
+    } 
+    
+    else {
+        // If wrong answer, we subtract 10 seconds from the timer 
+        timerCount - 10;
+        // If wrong answer, change background of the button to red
+        choiceButton.setAttribute("style", "background-color: red");
+        nextQuestion();
+}
+
+
+// If right or wrong answer, move on to the next question
+function nextQuestion(){
+    while(answerButton.firstchild) {
+        answerButton.removeChild(answerButton.firstChild);
+    }
+}
+
+// If time > 0, then score = timeLeft
+
+// If time = 0, and questions = left = failed test ==> start page
+
+// Function that stores the score and the name to the local storage: After all the questions are done, we see the score and the submit form 
+
+// Function that renders the scores and the name on the scoreboard from the local storage: After we submit, we the see the past scores
+
+// On the scoreboard, we see reset and return button
+
+// When the reset button is pressed, it clears local storage
+
+// When the return button is pressed, it goes back to the start button screen
+
+// When the scoreboard button is pressed, it shows the highscores
+//TODO make showscore fx
+//TODO make quizdone
+//TODO fail quiz
