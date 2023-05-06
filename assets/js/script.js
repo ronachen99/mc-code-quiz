5804801d21c8b866815714beeb1fda88cad0e64d
rona chen <rona.chen@hotmail.com>
2023-05-06 14:27:10 -0400

    changed the css and html file for the question container

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,13 @@
-
-var startButton = document.querySelector(".start-button");
-var displayQuestion = false;
-var question = document.querySelector(".quiz");
-
-
-var questions = ["1+1", "2+2"];
-var answers = [["2","3","4"], ["2","3","4"]];
+var startBtn = document.getElementById("#start-button");
+var timerCount = document.getElementById("#timer-count");
+var scoreCount = document.getElementById("#score-count");
 
 
-// Calls the startQuiz function when the start button is clicked
-startButton.addEventListener("click", startQuiz);
 
-// Starts timer and displays questions when the startQuiz function is called
-function startQuiz() {
-    timerCount = 50;
-    displayQuestion();
-    startTimer();
-} 
+
+
+
+
 
 // Function that diplays question
 
@@ -25,13 +16,4 @@ function startQuiz() {
 // Function that stores the score and the name to the local storage
 
 // Function that renders the scores and the name on the scoreboard from the local storage
-
-
-
-
-var state = questions.getAttribute("data-state");
-
-if (state === "hidden") {
-    
-}
 
